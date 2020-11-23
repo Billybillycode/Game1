6ae213169c034076bd9ed731ea8a6b338e79c647
Gwerborc'h Mahéo <gwerborch.maheo.com>
2020-11-23 12:41:48 +0100

    comit setup up

---
+++ js/world.js
@@ -0,0 +1,18 @@
+// Have to do export
+
+
+//1. create a Map
+
+let map = [
+    "                                                                           ",
+    "                     vvvvv                                                 ",
+    "                                                           vv              ",
+    "                              vvvvvvvvvv                                   ",
+    "    vvvvvvvvv                                                              ",
+    "                                                          vv               ",
+    "                                                                           ",
+    "                                                 vv                        ",
+    "                                                                           ",
+    "                                                                           ",
+]
+
